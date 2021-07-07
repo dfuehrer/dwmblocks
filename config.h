bf8003edd33a676a721329c5906f182c9acf5e66
dfuehrer <dfuehrer@email.arizona.edu>
2021-07-07 09:05:30 -0700

    updated delim

--- config.h
+++ config.h
@@ -19,7 +19,8 @@ static const Block blocks[] = {
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char *delim = " ";
+static char delimeter[] = "|";
+static char * delim = delimeter;
 
 // Have dwmblocks automatically recompile and run when you edit this file in
 // vim with the following line in your vimrc/init.vim:
